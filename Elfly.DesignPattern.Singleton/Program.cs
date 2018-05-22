9044f57539685fbac671de7d8debe896dbc6a420
FeiFeiJin <dsf_jyf@qq.com>
2018-05-22 15:43:32 +0800

    增加单例模式的实现。

---
+++ Elfly.DesignPattern.Singleton/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Elfly.DesignPattern.Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task[] taskArray = new Task[10];
+            for (int i = 0; i < taskArray.Length; i++)
+            {
+                taskArray[i] = Task.Factory.StartNew((Object obj) =>
+                {
+                    dynamic o = obj;
+                    int number = GetTicketNumber();
+                    Console.WriteLine("Task #{0},ThreadID #{1},票根编码 #{2}", o.number, Thread.CurrentThread.ManagedThreadId,number);
+                }, new {number = i });
+
+            }
+            Task.WaitAll(taskArray);
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// 获取票根编码
+        /// </summary>
+        /// <returns></returns>
+        private static int GetTicketNumber()
+        {
+            TicketMaker ticketMaker = TicketMaker.GetInstance();
+            int number = ticketMaker.GetNextTickerNumber();
+            return number;
+        }
+    }
+}
---
+++ Elfly.DesignPattern.Singleton/TicketMaker.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Elfly.DesignPattern.Singleton
+{
+    /// <summary>
+    /// 票根生成器
+    /// </summary>
+    public class TicketMaker
+    {
+        private int ticket = 1000;
+
+        /// <summary>
+        /// 静态的TicketMack
+        /// </summary>
+        private static TicketMaker _TicketMacker = null;
+
+        private static object obj = new object();
+        /// <summary>
+        /// 私有化构造函数，预防被初始化
+        /// </summary>
+        private TicketMaker()
+        {
+
+        }
+
+        /// <summary>
+        /// 获取实例的方法
+        /// </summary>
+        /// <returns>返回实例</returns>
+        public static TicketMaker GetInstance()
+        {
+            lock (obj)
+            {
+                if (_TicketMacker == null)
+                {
+                    _TicketMacker = new TicketMaker();
+                }
+            }
+            return _TicketMacker;
+        }
+
+        /// <summary>
+        /// 累加器方法
+        /// </summary>
+        /// <returns>增加后的值</returns>
+        public int GetNextTickerNumber()
+        {
+            return ticket++;
+        }
+    }
+}
